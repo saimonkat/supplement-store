125795db2195cf38b10064c0375841e678de3059
Saimon Kataev <saimon.kat23@gmail.com>
2025-08-15 23:54:59 +0200

    init commit

---
+++ src/components/pages/home/hero/hero.tsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <section className="hero py-32 px-safe">
+      <div className="container">
+        <div className="mx-auto flex max-w-[680px] flex-col gap-y-5">
+          <h1 className="fs-40 font-semibold">Welcome!</h1>
+          <p className="text-18 leading-snug tracking-tight sm:text-16">
+            to the Next.js TypeScript Tailwind Starter.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/shared/link/link.tsx
@@ -0,0 +1,70 @@
+import type { Route } from 'next';
+import NextLink from 'next/link';
+
+import clsx from 'clsx';
+
+const styles = {
+  transition: 'transition-colors duration-200',
+  base: 'inline-flex items-center',
+  size: {
+    md: 'text-16 tracking-tight',
+    sm: 'text-15 tracking-tight',
+    xs: 'text-14 tracking-tight',
+  },
+  theme: {
+    white: 'text-white hover:text-gray-80',
+    gray: 'text-gray-94 hover:text-gray-70',
+    black: 'text-black hover:text-gray-20',
+  },
+};
+
+type LinkProps<T extends string = string> = {
+  className?: string;
+  href: Route<T> | URL;
+  size?: keyof typeof styles.size;
+  theme?: keyof typeof styles.theme;
+  children: React.ReactNode;
+  prefetch?: boolean;
+  target?: string;
+  rel?: string;
+  withArrow?: boolean;
+  title?: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+};
+
+function Link({
+  className: additionalClassName,
+  size,
+  theme,
+  href,
+  children,
+  ...props
+}: LinkProps) {
+  const linkClassName = clsx(
+    styles.transition,
+    size && theme && styles.base,
+    size && styles.size[size],
+    theme && styles.theme[theme],
+    additionalClassName,
+  );
+
+  /*
+    Using next/link component only for internal navigation.
+    https://github.com/vercel/next.js/blob/canary/errors/invalid-href-passed.md
+  */
+  if (href.toString().startsWith('/')) {
+    return (
+      <NextLink className={linkClassName} href={href} {...props}>
+        {children}
+      </NextLink>
+    );
+  }
+
+  return (
+    <a className={linkClassName} href={href.toString()} {...props}>
+      {children}
+    </a>
+  );
+}
+
+export default Link;
